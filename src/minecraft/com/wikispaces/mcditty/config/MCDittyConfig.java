2ba5ccfc8c0d4708da8ac53997d19faf9c4c7591
fenceFoil <noemailyet@notyet.com>
2013-01-14 10:18:47 -0500

    Incremented version to 0.9.9.02

--- src/minecraft/com/wikispaces/mcditty/config/MCDittyConfig.java
+++ src/minecraft/com/wikispaces/mcditty/config/MCDittyConfig.java
@@ -59,7 +59,7 @@ public class MCDittyConfig {
 	/**
 	 * Current MCDitty version.
 	 */
-	public static final String CURRENT_VERSION = "0.9.9.01";
+	public static final String CURRENT_VERSION = "0.9.9.02";
 	/**
 	 * Minecraft version that the mod is designed for.
 	 */
