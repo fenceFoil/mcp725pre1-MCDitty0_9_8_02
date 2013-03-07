3b0f9ee88c4d3d81134e04ef62924b98b0ac0257
fenceFoil <noemailyet@notyet.com>
2013-03-07 17:01:23 -0500

    Fixed (?) signs appearing as normal TileEntitySign on first load (a delay was changed from 1000 to 200ms)

--- src/minecraft/com/minetunes/Minetunes.java
+++ src/minecraft/com/minetunes/Minetunes.java
@@ -467,7 +467,7 @@ public class Minetunes {
 			public void run() {
 				while (true) {
 					try {
-						Thread.sleep(1000);
+						Thread.sleep(200);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
