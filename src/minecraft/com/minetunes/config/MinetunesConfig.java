90987b32fc579df6ae0cfb02a94e9f076b322372
fenceFoil <noemailyet@notyet.com>
2013-03-08 16:45:45 -0500

    Incremented version to 0.9.12

--- src/minecraft/com/minetunes/config/MinetunesConfig.java
+++ src/minecraft/com/minetunes/config/MinetunesConfig.java
@@ -50,12 +50,12 @@ public class MinetunesConfig {
 	/**
 	 * Current MineTunes version.
 	 */
-	public static final String CURRENT_VERSION = "0.9.11.00";
+	public static final String CURRENT_VERSION = "0.9.12.00";
 	/**
 	 * Minecraft version that the mod is designed for.
 	 */
 	public static final String MC_CURRENT_VERSION = "1.4.6";
-	private static final String[] UPDATE_MESSAGE = { "§aNew:§r Upgraded MineTunes menu -- now with help!" };
+	private static final String[] UPDATE_MESSAGE = { "§aNew:§r Book keyword." };
 	/**
 	 * Disable to stop costly printlns from being called
 	 */
